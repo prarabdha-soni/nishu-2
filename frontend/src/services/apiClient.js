0a0a9b3b03b760d04f1f3b4c8489a54325e1f672
prarabdha-soni <prarabdha21@gmail.com>
2025-09-17 14:08:04 +0000

    init

--- frontend/src/services/apiClient.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'https://nishu-2.onrender.com';
-
-export const apiClient = axios.create({
-  baseURL: API_BASE_URL,
-  headers: { 'Content-Type': 'application/json' }
-}); 
